77247504d978b44a3a28e6dac52f0aa8386c92a5
Fermin Da Cruz <ferminjuliancruz26@gmail.com>
2024-07-03 20:27:55 -0300

    advertencia agregar al carrito

--- JS/pre-entrega5-DaCruz.js
+++ JS/pre-entrega5-DaCruz.js
@@ -33,8 +33,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const cuotasSelect = document.getElementById('cuotas');
     const resultadoDiv = document.getElementById('resultado');
     const productosContainer = document.getElementById('productos-container');
+    const iphonesJSON = 'iphones.json'
     
-    fetch('iphones.json')
+    fetch(iphonesJSON)
         .then(response => response.json())
         .then(data => {
             const iPhones = data.map(item => new Iphone(item.modelo, item.precio));
@@ -45,84 +46,143 @@ document.addEventListener("DOMContentLoaded", () => {
             const DOMTotal = document.getElementById('total');
             const DOMBotonVaciar = document.getElementById('boton-vaciar');
 
-
-
-
-
             const renderizarProductos = (info) => {
                 const html = `
-                                <div class="tarjeta-productos">
-                                    <div>
-                                        <img src="${info.imagen}" alt="imagen-producto">
-                                        <h3>${info.modelo.replace(/\_/g, ' ')}</h3>
-                                        <p>$${info.precio}</p>
-                                        <button class="boton-comprar" marcador="${info.id}">comprar</button>
-                                    </div>
-                                </div>
-                                `;
+                    <div class="tarjeta-productos">
+                        <div>
+                            <img src="${info.imagen}" alt="imagen-producto">
+                            <h3>${info.modelo.replace(/\_/g, ' ')}</h3>
+                            <p>$${info.precio}</p>
+                            <button class="boton-comprar" marcador="${info.id}">comprar</button>
+                        </div>
+                    </div>
+                `;
                 DOMItems.innerHTML += html;
                 document.querySelectorAll('.boton-comprar').forEach((boton) => {
                     boton.addEventListener('click', agregarProductoCarrito)
-                })
-            }
+                });
+            };
 
             const agregarProductoCarrito = (evento) => {
-                carrito.push(evento.target.getAttribute('marcador'))
-                localStorage.setItem('carrito', JSON.stringify(carrito))
-                Swal.fire({
-                    title: "agregado al carrito",
-                    text: "el producto fue agregado al carrito",
-                    icon: "success",
-                    width: 400,
-                    timer: 2000,
-                    showConfirmButton: false,
-                    position: 'top-end',
-                    backdrop: false,
-                    customClass: {
-                        title: 'swal2-title',
-                        content: 'swal2-content'
-                    }
-                });
-                renderizarCarrito();
-            }
+                const id = evento.target.getAttribute('marcador');
+                verificarDisponibilidadProducto(id, carrito)
+                    .then(() => {
+                        carrito.push(id);
+                        localStorage.setItem('carrito', JSON.stringify(carrito));
+                        Swal.fire({
+                            title: "agregado al carrito",
+                            text: "el producto fue agregado al carrito",
+                            icon: "success",
+                            width: 400,
+                            timer: 2000,
+                            showConfirmButton: false,
+                            position: 'top-end',
+                            backdrop: false,
+                            customClass: {
+                                title: 'swal2-title',
+                                content: 'swal2-content'
+                            }
+                        });
+                        renderizarCarrito();
+                    })
+                    .catch(error => {
+                        Swal.fire({
+                            title: "Producto ya está en el carrito",
+                            text: "¿Desea agregarlo de nuevo?",
+                            icon: "warning",
+                            showCancelButton: true,
+                            confirmButtonText: "Agregar de nuevo",
+                            cancelButtonText: "Cancelar",
+                            customClass: {
+                                title: 'swal2-title',
+                                content: 'swal2-content'
+                            }
+                        }).then((resultado) => {
+                            if (resultado.isConfirmed) {
+                                carrito.push(id);
+                                localStorage.setItem('carrito', JSON.stringify(carrito));
+                                Swal.fire({
+                                    title: "Producto agregado al carrito",
+                                    text: "El producto fue agregado al carrito exitosamente",
+                                    icon: "success",
+                                    width: 400,
+                                    timer: 2000,
+                                    showConfirmButton: false,
+                                    position: 'top-end',
+                                    backdrop: false,
+                                    customClass: {
+                                        title: 'swal2-title',
+                                        content: 'swal2-content'
+                                    }
+                                });
+                            } else {
+                                Swal.fire({
+                                    title: "Cancelado",
+                                    text: "El producto no fue agregado al carrito",
+                                    icon: "info",
+                                    width: 400,
+                                    timer: 2000,
+                                    showConfirmButton: false,
+                                    position: 'top-end',
+                                    backdrop: false,
+                                    customClass: {
+                                        title: 'swal2-title',
+                                        content: 'swal2-content'
+                                    }
+                                });
+                            }
+                        })
 
-            function renderizarCarrito () {
+
+                    });
+            };
+
+            const renderizarCarrito = () => {
                 DOMCarrito.innerHTML = ``; // Para no repetir productos
                 carrito.forEach((id) => {
                     // busca el ID y coloca el objeto que coincide en producto
-                    const producto = data.find((info) => info.id === parseInt(id)) 
+                    const producto = data.find((info) => info.id === parseInt(id));
                     const html = `
                     <li>
                         <p>${producto.modelo.replace(/\_/g, ' ')}</p> - <p>$${producto.precio}</p>
-                    </li>`
+                    </li>`;
                     DOMCarrito.innerHTML += html;
-                })
-                const total = carrito.reduce((total,id) => {
-                    const producto = data.find((info) => info.id === parseInt(id))
+                });
+                const total = carrito.reduce((total, id) => {
+                    const producto = data.find((info) => info.id === parseInt(id));
                     return total + producto.precio;
-                },0)
-                DOMTotal.textContent = total.toFixed(2)
+                }, 0);
+                DOMTotal.textContent = total.toFixed(2);
             }
+
             const vaciarCarrito = () => {
                 carrito = [];
                 localStorage.removeItem('carrito');
                 renderizarCarrito();
-            }
-            
-            
+            };
 
-            // Llamos a las funciones
+            const verificarDisponibilidadProducto = (id, carrito) => {
+                return new Promise((resolve, reject) => {
+                    const disponible = !carrito.includes(id);
+                    
+                    disponible ? resolve() : reject(`Producto ya esta en el carrito`); 
+                });
+            }
+
+            // Llamar a las funciones
             cargarOpciones(iPhones, modeloSelect);
             cargarSeleccionPrevia(modeloSelect, cuotasSelect);
             configurarEventos(iPhones, modeloSelect, cuotasSelect, resultadoDiv);
             data.forEach((info) => {
                 renderizarProductos(info);
             });
-            DOMBotonVaciar.addEventListener('click', vaciarCarrito)
+            DOMBotonVaciar.addEventListener('click', vaciarCarrito);
             renderizarCarrito();
         })
-})
-
+        .catch(error => {
+            console.error('Error al cargar los datos:', error);
+        });
+});
 // Funciones
 const cargarOpciones = (iPhones, modeloSelect) => {
     iPhones.forEach(iphone => {
